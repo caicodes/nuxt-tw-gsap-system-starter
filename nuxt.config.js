b5db1f1d243c463ece14d40fa2415881649c1a67
Cai Codes <caicodes1@gmail.com>
2021-09-22 14:09:11 -0600

    set up stage

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ascend-notifications',
+    title: 'nuxt-tw-gsap-system-starter',
     htmlAttrs: {
       lang: 'en',
     },
